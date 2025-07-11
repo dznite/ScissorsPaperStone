b1d06b78efb64705bbcdc3f38d630d0001bb93a3
dznite <122264113+dznite@users.noreply.github.com>
2025-07-11 17:47:20 +0800

    Create getComputerChoice function
    Computer is able to return either a Rock, Paper or Scissors with this function

--- blank.js
+++ blank.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+console.log("Hello world");
+
+function getComputerChoice(){
+    var rng = Math.random();
+    if (rng <= 0.33){
+        return "Rock";
+    } else if (rng > 0.33 && rng <= 0.66){
+        return "Paper";
+    } else if (rng > 0.66){
+        return "Scissors"
+    }
+}
+
+console.log(getComputerChoice());
